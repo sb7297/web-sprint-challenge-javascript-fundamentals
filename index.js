17126f8d2536ed83d511b2bcd8b110b61cfc6919
sb7297 <sbaireddy7297@gmail.com>
2021-06-18 16:11:26 -0400

    index.js: rewrite summation with a closure

--- index.js
+++ index.js
@@ -33,11 +33,14 @@ variables even though they may not be passed in to the function.
 For example, `summation(4)` should return 10 because 1+2+3+4 is 10. Note, you may use a for loop for this function if you wish */
 
 function summation(num) {
-  let ret = 0;
-  for (let i = num; i > 0; i--) {
-    ret += i;
-  }
-  return ret;
+  let counter = function() {
+    if (num > 0) {
+      return num-- + counter();
+    }
+    else return 0;
+  };
+
+  return counter();
 }
  
 
